5648597fc3334f888e1babe016c9d5c410fea10e
jakka4321 <134845800+jakka4321@users.noreply.github.com>
2023-06-21 20:10:53 +0530

    Add files via upload

---
+++ setoperation.py
@@ -0,0 +1,10 @@
+E={0,2,4,6,8}
+N={1,2,3,4,5}
+#union of E and N is
+print("union of E and N is",E|N)
+#intersection of E and N
+print("intersection of E and N is",E&N)
+#difference of E and N
+print("difference of E and N is",E-N)
+#symmetric difference of E and N
+print("symmetric difference of E and N is",E^N)
